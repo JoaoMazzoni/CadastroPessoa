089a615264caa5f7d5775c9d6fc843a4da41bd39
JoaoMazzoni <joao.mazzoni@5by5.com>
2024-04-29 12:09:34 -0300

    Add project files.

---
+++ CadastroPessoa/Pessoa.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Pessoa
+{
+    public string Nome;
+    public string Sobrenome;
+    public int Idade;
+    public string Telefone;
+    public string Celular;
+    public string Email;
+
+
+    public Pessoa()
+    {
+    
+
+    }
+
+    public string definirNome() //Com retorno
+    {
+        Console.WriteLine("\n-------------------------------------");
+        Console.WriteLine("Informe o nome: "); 
+        return Console.ReadLine();
+    }
+    public string definirSobrenome() //Com retorno
+    {
+        Console.WriteLine("Informe o sobrenome: "); 
+        return Console.ReadLine();
+    }
+
+    public int definirIdade() //Com retorno
+    {
+        Console.WriteLine("Informe a Idade: "); 
+        return int.Parse(Console.ReadLine());
+    }
+
+    public void definirTelefone(string telefone) //Com parâmetro
+    {
+        this.Telefone = telefone; //Usando "this"
+    }
+
+    public void definirCelular(string celular) //Com parâmetro
+    {
+        this.Celular = celular;
+
+    }
+
+    public void definirEmail(string email) //Com parâmetro
+    { 
+        this.Email = email;
+    }
+    
+    public void mostrarDados()
+    {
+        Console.WriteLine($"\n\nNome: {Nome}", this.Nome);
+        Console.WriteLine($"Sobrenome: {Sobrenome}", this.Sobrenome);
+        Console.WriteLine("Idade: " + this.Idade);
+        Console.WriteLine("Telefone: " + this.Telefone);
+        Console.WriteLine($"Celular: {Celular}", this.Celular);
+        Console.WriteLine($"Email: {Email}", this.Email);
+
+    }
+}
---
+++ CadastroPessoa/Program.cs
@@ -0,0 +1,41 @@
+﻿
+string telefone, celular, email;
+Pessoa pessoa1 = new Pessoa();
+Pessoa pessoa2 = new Pessoa();
+Pessoa pessoa3 = new Pessoa();
+
+Pessoa criarPessoa()
+{
+    Pessoa pessoa = new Pessoa();
+    pessoa.Nome = pessoa.definirNome(); //Sem parâmetros com retorno
+    pessoa.Sobrenome = pessoa.definirSobrenome(); // Sem parâmetros com retorno
+    pessoa.Idade = pessoa.definirIdade(); //Sem parâmetros com retorno
+
+    Console.WriteLine("Informe o telefone: "); //Com parêmetros usando "this"
+    telefone = Console.ReadLine();
+    pessoa.definirTelefone(telefone);
+
+    Console.WriteLine("Informe o celular: "); //Com parêmetros usando "this"
+    celular = Console.ReadLine();
+    pessoa.definirCelular(celular);
+
+
+    Console.WriteLine("Informe o email: ");//Com parêmetros usando "this"
+    email = Console.ReadLine();
+    pessoa.definirEmail(email);
+
+    return pessoa;
+}
+
+pessoa1 = criarPessoa();
+pessoa2 = criarPessoa();
+pessoa3 = criarPessoa();
+
+Console.WriteLine("\nPrimeiro cadastro: ");
+pessoa1.mostrarDados();
+
+Console.WriteLine("\nSegundo cadastro: ");
+pessoa2.mostrarDados();
+
+Console.WriteLine("\nTerceiro cadastro: ");
+pessoa3.mostrarDados();
